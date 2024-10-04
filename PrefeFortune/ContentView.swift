e62fa77b85d692eb591760366a229e6cac0bb286
irukahaoruka <erika03301109@gmail.com>
2024-10-04 11:09:47 +0900

    add initialfiles

---
+++ PrefeFortune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrefeFortune
+//
+//  Created by 櫻井絵理香 on 2024/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrefeFortune/PrefeFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefeFortuneApp.swift
+//  PrefeFortune
+//
+//  Created by 櫻井絵理香 on 2024/10/04.
+//
+
+import SwiftUI
+
+@main
+struct PrefeFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
