eb620c84c64c15003237ffc8fb4dd8918ed29b87
irukahaoruka <erika03301109@gmail.com>
2024-10-04 16:12:45 +0900

    feat: テスト用のContentViewの追加

--- PrefeFortune/Views/ContentView.swift
+++ PrefeFortune/Views/ContentView.swift
@@ -8,16 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    let fortuneAPIManager: FortuneAPIManager
-    let birthday = YearMonthDay(year: 2000, month: 1, day: 27)
-    let today = YearMonthDay(year: 2023, month: 5, day: 5)
+    @StateObject private var fortuneManager = FortuneAPIManager()
 
     var body: some View {
         VStack {
-            Text("テスト用")
-        }
-        .onAppear {
-            fortuneAPIManager.fetchFortune(name: "sakurai", birthday: birthday, bloodType: "AB", today: today)
+            
+            if fortuneManager.isLoading {
+                ProgressView()
+            } else if let fortune = fortuneManager.fortuneResponse {
+                VStack(alignment: .leading) {
+                    Text("名前: \(fortune.name)")
+                    Text("都市: \(fortune.capital)")
+                    if let citizenDay = fortune.citizenDay {
+                        Text("zitizenDay: \(citizenDay.month)/\(citizenDay.day)")
+                    }
+                    Text("詳細: \(fortune.brief)")
+                }
+                .padding()
+            }
+
+            Button(action: {
+                fortuneManager.fetchFortune(
+                    name: "ゆめみ",
+                    birthday: YearMonthDay(year: 2000, month: 1, day: 27),
+                    bloodType: "ab",
+                    today: YearMonthDay(year: 2023, month: 5, day: 5)
+                )
+            }) {
+                Text("占う")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .disabled(fortuneManager.isLoading)
         }
     }
 }
