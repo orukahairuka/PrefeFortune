10b7d6b8002b5b8a426bcb891bd1c091d6d25637
irukahaoruka <erika03301109@gmail.com>
2024-10-07 14:46:36 +0900

    rafactor: ファイルの構成を変更

---
+++ PrefeFortune/Views/FortuneResult/PrefectureImage/PrefectureImageView.swift
@@ -0,0 +1,32 @@
+//
+//  PrefectureImageView.swift
+//  PrefeFortune
+//
+//  Created by 櫻井絵理香 on 2024/10/04.
+//
+
+import SwiftUI
+
+struct PrefectureImageView: View {
+    @Binding var imageUrl: URL?
+
+    var body: some View {
+        GeometryReader { geometry in
+            if let validUrl = imageUrl {
+                ZStack {
+                        ZStack {
+                            Text("a")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            AsyncImageLoadingView(url: validUrl)
+                                .frame(width: geometry.size.width, height: geometry.size.height * 0.9)
+                                .clipped()
+                        }
+                    ClackerAnimationView(lottieFile: "ClackerAnimation")
+                }
+            }
+        }
+        .frame(height: UIScreen.main.bounds.height * 0.4)
+    }
+}
+
