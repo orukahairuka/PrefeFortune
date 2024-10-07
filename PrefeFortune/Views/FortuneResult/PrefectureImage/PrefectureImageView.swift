360c25ed6cd1721985a90750d0e72bc9baa1ca03
irukahaoruka <erika03301109@gmail.com>
2024-10-07 15:03:21 +0900

    feat: 県名を表示させた

--- PrefeFortune/Views/FortuneResult/PrefectureImage/PrefectureImageView.swift
+++ PrefeFortune/Views/FortuneResult/PrefectureImage/PrefectureImageView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct PrefectureImageView: View {
     @Binding var imageUrl: URL?
+    @Binding var prefectureName: String?
 
     var body: some View {
         GeometryReader { geometry in
             if let validUrl = imageUrl {
                 ZStack {
-                    Text("a")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    AsyncImageLoadingView(url: validUrl)
-                        .frame(width: geometry.size.width, height: geometry.size.height * 0.9)
-                        .clipped()
+                    VStack {
+                        if let name = prefectureName {
+                            Text(name)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                        }
+                        AsyncImageLoadingView(url: validUrl)
+                            .frame(width: geometry.size.width, height: geometry.size.height * 0.9)
+                            .clipped()
+                    }
                     ClackerAnimationView(lottieFile: "ClackerAnimation")
                 }
             }
