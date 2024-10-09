27a66b1096a24ceca0caf400d264e6a5900269ed
irukahaoruka <erika03301109@gmail.com>
2024-10-09 13:00:25 +0900

    feat: loading画面の実装

--- PrefeFortune/PrefeFortuneApp.swift
+++ PrefeFortune/PrefeFortuneApp.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 @main
 struct PrefeFortuneApp: App {
-    @StateObject var fortuneAPIManager = FortuneAPIManager()
-    @StateObject var placesAPIManager = PlacesAPIManager()
-    @StateObject var latLogAPIManager = LatLonManager()
+    @StateObject var stateManager = StateManager()
+
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                UserInfoInputView(
-                    fortuneAPIManager: fortuneAPIManager
-                )
+            if stateManager.isLocationLoading {
+                // ローディング中はLanchLoadingViewを表示
+                LanchLoadingView()
+                    .onAppear {
+                        // 位置情報の取得を開始
+                        stateManager.setupLocationManager()
+                    }
+            } else {
+                // ローディング完了後にStartNavigationViewを表示
+                NavigationStack {
+                    StartNavigationView()
+                        .environmentObject(stateManager) // StateManagerを共有
+                }
             }
         }
     }
