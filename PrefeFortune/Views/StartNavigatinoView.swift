0066a4b74061bb5d7a46cd833783f2907102b436
irukahaoruka <erika03301109@gmail.com>
2024-10-09 13:04:28 +0900

    feat: 画面遷移のフラグ管理

--- PrefeFortune/Views/StartNavigatinoView.swift
+++ PrefeFortune/Views/StartNavigatinoView.swift
@@ -17,59 +17,69 @@ struct StartNavigationView: View {
     @State private var currentCommentIndex = 0
     @State private var buttonLabel = "Tap"
     @State private var tapCount = 0
-    @State private var navigateToInputView = false // 画面遷移のフラグ
+    @State private var navigateToInputView = false
+    @AppStorage("hasSeenTutorial") private var hasSeenTutorial: Bool = false
 
     var body: some View {
-        NavigationStack {
-            VStack {
-                ZStack {
-                    Image("PopUpImage2")
-                        .resizable()
-                        .frame(width: 360, height: 220)
-                        .scaledToFit()
-                        .scaleEffect(x: -1, y: 1)
-                        .opacity(0.9)
+        if !hasSeenTutorial {
+            NavigationStack {
+                VStack {
+                    ZStack {
+                        Image("PopUpImage2")
+                            .resizable()
+                            .frame(width: 360, height: 220)
+                            .scaledToFit()
+                            .scaleEffect(x: -1, y: 1)
+                            .opacity(0.9)
+                            .shadow(color: .black.opacity(0.3), radius: 10, x: 1, y: 1)
+
+                        Text(displayedText)
+                            .fontWeight(.bold)
+                            .frame(width: 360, height: 210)
+                            .multilineTextAlignment(.center)
+                            .transition(.opacity)
+                    }
+                    .offset(y: 30)
+
+                    CatTypeAnimationView(lottieFile: "CatTypeAnimation")
+                        .frame(width: 400, height: 300)
+                        .padding(.horizontal, 10)
                         .shadow(color: .black.opacity(0.3), radius: 10, x: 1, y: 1)
 
-                    Text(displayedText)
-                        .fontWeight(.bold)
-                        .frame(width: 360, height: 210)
-                        .multilineTextAlignment(.center)
-                        .transition(.opacity)
-                }
-                .offset(y: 30)
-
-                CatTypeAnimationView(lottieFile: "CatTypeAnimation")
-                    .frame(width: 400, height: 300)
-                    .padding(.horizontal, 10)
-                    .shadow(color: .black.opacity(0.3), radius: 10, x: 1, y: 1)
-
-                Button(action: {
-                    withAnimation {
-                        changeText()
+                    Button(action: {
+                        withAnimation {
+                            changeText()
+                        }
+                    }) {
+                        Text(buttonLabel)
+                            .padding()
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .frame(width: 200, height: 100)
+                            .background(Color.customYellowColor)
+                            .foregroundColor(.white)
+                            .cornerRadius(50)
                     }
-                }) {
-                    Text(buttonLabel)
-                        .padding()
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .frame(width: 200, height: 100)
-                        .background(Color.customYellowColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(50)
+                    .opacity(0.8)
+                    .padding(.top, 20)
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 3, y: 3)
                 }
-                .opacity(0.8)
-                .padding(.top, 20)
-                .shadow(color: .black.opacity(0.3), radius: 10, x: 3, y: 3)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Color.customRadialGradient
+                        .ignoresSafeArea()
+                )
+                .navigationDestination(isPresented: $navigateToInputView) {
+                    UserInfoInputView()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EmptyView()
+                    }
+                }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                Color.customRadialGradient
-                    .ignoresSafeArea()
-            )
-            .navigationDestination(isPresented: $navigateToInputView) {
-                UserInfoInputView()
-            }
+        } else {
+            UserInfoInputView()
         }
     }
 
@@ -87,9 +97,10 @@ struct StartNavigationView: View {
         tapCount += 1
 
         if tapCount == 3 {
-            buttonLabel = "OK" // OKボタンに変更
+            buttonLabel = "OK"
         } else if tapCount == 4 {
-            navigateToInputView = true // 次のタップで画面遷移
+            navigateToInputView = true
+            hasSeenTutorial = true
         }
     }
 }
